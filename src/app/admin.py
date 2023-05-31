6df446953d82a69a6a3d08216dc88c9631a806fa
yura34054 <ur34054@gmail.com>
2023-05-31 10:52:43 +0300

    added basic post model

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,3 +1,2 @@
 from django.contrib import admin
-
-# Register your models here.
+from app.internal.admin.post_admin import PostAdmin
---
+++ src/app/internal/models/post.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.fields.CharField(max_length=64)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
