ac3098e35d9bd36fbdd5660c73a2112c2e243d93
yura34054 <ur34054@gmail.com>
2023-06-15 11:23:42 +0300

    added some stolen css

--- src/app/internal/transport/views.py
+++ src/app/internal/transport/views.py
@@ -14,7 +14,8 @@ def blog(request):
     return HttpResponse(render(request, "blog.html", context))
 
 
-def get_post(request, post_name):
-    template = select_template([f"{post_name}.html", "post_not_found.html"])
+def get_post(request, post_slug):
+    template = select_template([f"posts/{post_slug}.html", "post_not_found.html"])
+    context = {"post_info": Post.objects.filter(slug=post_slug).first()}
 
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(context))
--- src/app/internal/urls.py
+++ src/app/internal/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app.internal.transport import views
 
 urlpatterns = [
-    path("", views.index),
-    path("blog/", views.blog),
-    path("blog/<str:post_name>", views.get_post),
+    path("", views.index, name='index'),
+    path("blog/", views.blog, name='blog'),
+    path("blog/<str:post_slug>", views.get_post),
 ]
