c4399bdcd60da402962958290f1c6e18d598fb78
yura34054 <ur34054@gmail.com>
2023-06-29 12:46:16 +0300

    fixed blog behaving awful on mobile

--- src/app/internal/urls.py
+++ src/app/internal/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app.internal.transport import views
 
 urlpatterns = [
-    path("", views.index, name='index'),
-    path("blog/", views.blog, name='blog'),
+    path("", views.index, name="index"),
+    path("blog/", views.blog, name="blog"),
     path("blog/<str:post_slug>", views.get_post),
 ]
